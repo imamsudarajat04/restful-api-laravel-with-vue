d8899e99969a259fb432682d5fcd46c6cf4a95ac
imamsudarajat04 <imamsudarajat708@gmail.com>
2024-01-08 16:16:17 +0700

    feat: data dummy for table posts

--- backend-restfull-api/routes/api.php
+++ backend-restfull-api/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PostsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::resource('posts', PostsController::class);
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
